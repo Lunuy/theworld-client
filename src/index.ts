74c0ad0e56cd0fa87291d29e0c6f46aff308620e
Lunuy <luminuy@gmail.com>
2022-09-24 13:22:21 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@
 import { Broadcaster } from './Broadcaster';
 import { Client } from './Client';
 import Field from './Field';
+import { Plugin } from './Plugin';
 
 export {
     Client,
     Field,
-    Broadcaster
+    Broadcaster,
+    Plugin
 };
